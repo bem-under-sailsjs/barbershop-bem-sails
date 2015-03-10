8719c3825e7150f345c834b515a3a3dacf07f73a
alexbaum <alexbaum@yandex-team.ru>
2015-03-10 12:15:10 +0300

    Pretty output product-card layout

--- views/common.blocks/product-card/product-card.deps.js
+++ views/common.blocks/product-card/product-card.deps.js
@@ -1,3 +1,34 @@
 ({
-    shouldDeps: []
+    shouldDeps: [
+        {
+            elems: [
+                'header',
+                'info-header',
+                'image',
+                'theme',
+                'annotation',
+                'content',
+                'isbn',
+                'balance',
+                'e-buy-info',
+                'spreads',
+                'reviews',
+                'publish-info',
+                'columns',
+                'price',
+                'buy'
+            ]
+        },
+        {
+            elem: 'column',
+            mods: { position: ['left', 'center', 'right'] }
+        },
+        {
+            block: 'image'
+        },
+        {
+            block: 'button',
+            mods: {type: 'buy'}
+        }
+    ]
 });
