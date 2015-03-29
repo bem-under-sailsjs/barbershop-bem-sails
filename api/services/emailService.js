c3c5f1df575c4a17d64fbac08f392d7f4e21e6fc
alexbaum <alexbaum@yandex-team.ru>
2015-03-29 18:37:44 +0300

    Send order letter to manages as well as to customer

--- api/services/emailService.js
+++ api/services/emailService.js
@@ -3,15 +3,20 @@ var path = require('path');
 
 var Mailgun = require('machinepack-mailgun');
 var ejs = require('ejs');
+var async = require('async');
+
 var template = ejs.compile(fs.readFileSync(path.resolve(__dirname, 'email-templates', 'order.ejs'), 'utf-8'));
 
 module.exports = {
 
     /*
-     * Send emails by mailgun API
-     * @see https://mailgun.com/
+     * Send emails by mailgun API with machinepack-mailgun
+     *
+     * @see https://documentation.mailgun.com
+     * @see http://node-machine.org/machinepack-mailgun
      *
      * @param {Object} params
+     * @param {Function} callback
      */
     sendEmail: function(params, callback) {
         var order = params.order,
@@ -30,31 +35,43 @@ module.exports = {
                     htmlMessage = template({order: order});
                 } catch(e) {}
 
-                // Send an html email.
-                Mailgun.sendHtmlEmail({
-
+                var mailParam = {
                     apiKey: sails.config.email.apiKey,
                     domain: sails.config.email.domain,
-                    toEmail: 'alexbaumgertner@yandex.ru',
-                    toName: order.name,
+
+                    toEmail: sails.config.manager.email,
+                    toName: sails.config.manager.name,
+
                     subject: 'Заказ номер: ' + order.cartID,
 
                     textMessage: textMessage,
                     htmlMessage: htmlMessage,
 
-                    fromEmail: 'alex.baumgertner@gmail.com',
-                    fromName: 'Alex Baumgertner'
+                    fromEmail: sails.config.manager.email,
+                    fromName: sails.config.manager.name
+                };
 
-                }).exec({
-                    // An unexpected error occurred.
-                    error: function(err) {
-                        console.log('Mailgun err: ', err);
+                async.parallel({
+                    'mailToCustomer': function(callback) {
+                        mailParam.toEmail = order.email;
+                        mailParam.toName = order.name;
+
+                        Mailgun.sendHtmlEmail(mailParam).exec({
+                            error: function(err) {callback(err);},
+                            success: function(data) {callback(null, data);}
+                        });
+
                     },
-                    // OK.
-                    success: function(data) {
-                        console.log('Mailgun data: ', data);
-                        callback();
+                    'mailToManager': function(callback) {
+                        mailParam.subject = 'Сделан заказ номер: ' + order.cartID;
+
+                        Mailgun.sendHtmlEmail(mailParam).exec({
+                            error: function(err) {callback(err);},
+                            success: function(data) {callback(null, data);}
+                        });
                     }
+                }, function(err, res) {
+                    callback(err, res);
                 });
 
             }
