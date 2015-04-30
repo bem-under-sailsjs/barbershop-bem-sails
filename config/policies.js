ca0c15075dd078af0036b6d703d8a016e656db25
alexbaum <alexbaum@yandex-team.ru>
2015-04-30 17:16:27 +0300

    Set politics

--- config/policies.js
+++ config/policies.js
@@ -1,38 +1,17 @@
 /**
  * Policy Mappings
- * (sails.config.policies)
- *
- * Policies are simple functions which run **before** your controllers.
- * You can apply one or more policies to a given controller, or protect
- * its actions individually.
- *
- * Any policy file (e.g. `api/policies/authenticated.js`) can be accessed
- * below by its filename, minus the extension, (e.g. "authenticated")
- *
- * For more information on how policies work, see:
  * http://sailsjs.org/#/documentation/concepts/Policies
- *
- * For more information on configuring policies, check out:
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.policies.html
  */
-
-
 module.exports.policies = {
-
-    /***************************************************************************
-     *                                                                          *
-     * Default policy for all controllers and actions (`true` allows public     *
-     * access)                                                                  *
-     *                                                                          *
-     ***************************************************************************/
 
     '*': ['passport'],
 
     ProductController: {
         '*': ['passport', 'isAdmin'],
-        addToCart: 'passport',
-        show: 'passport',
-        index: 'passport'
+        'addToCart': 'passport',
+        'show': 'passport',
+        'index': 'passport'
     },
 
     UserController: {
@@ -40,26 +19,18 @@ module.exports.policies = {
         'show': ['passport', 'isAdminOrOwner'],
         'edit': ['passport', 'isAdminOrOwner'],
         'update': ['passport', 'isAdminOrOwner']
+    },
+
+    CartController: {
+        'edit': ['passport', 'isAdminOrOwner'],
+        'delete': ['passport', 'isAdmin']
+    },
+
+    PageController: {
+        'edit': ['passport', 'isAdmin'],
+        'create': ['passport', 'isAdmin'],
+        'new': ['passport', 'isAdmin'],
+        'update': ['passport', 'isAdmin'],
+        'delete': ['passport', 'isAdmin']
     }
-
-    /***************************************************************************
-     *                                                                          *
-     * Here's an example of mapping some policies to run before a controller    *
-     * and its actions                                                          *
-     *                                                                          *
-     ***************************************************************************/
-    // RabbitController: {
-
-    // Apply the `false` policy as the default for all of RabbitController's actions
-    // (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-    // '*': false,
-
-    // For the action `nurture`, apply the 'isRabbitMother' policy
-    // (this overrides `false` above)
-    // nurture	: 'isRabbitMother',
-
-    // Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-    // before letting any users feed our rabbits
-    // feed : ['isNiceToAnimals', 'hasRabbitFood']
-    // }
 };
